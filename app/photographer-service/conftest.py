480a81236b761b740172137aa04e9357bb9bac70
Franco Liberali <franco.liberali@imt-atlantique.net>
2021-10-12 14:49:14 +0200

    try to conenect to mongo

--- app/photographer-service/conftest.py
+++ app/photographer-service/conftest.py
@@ -12,5 +12,5 @@ def clearPhotographers():
 
 @pytest.fixture(scope="class")
 def initDB():
-    connect("photographers_test", host="mongo-service-test")
+    connect("photographers_test", host="mongo")
     yield
