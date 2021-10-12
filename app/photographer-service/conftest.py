06163d07469481fd106d4708e6c0ad01461455dc
Franco Liberali <franco.liberali@imt-atlantique.net>
2021-10-12 14:57:22 +0200

    Revert "try with mongodb"
    This reverts commit 46c58a06607aa393d9505d7398101608db3e03b0.

--- app/photographer-service/conftest.py
+++ app/photographer-service/conftest.py
@@ -12,5 +12,5 @@ def clearPhotographers():
 
 @pytest.fixture(scope="class")
 def initDB():
-    connect("photographers_test", host="mongodb")
+    connect("photographers_test", host="mongo")
     yield
